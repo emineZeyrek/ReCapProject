fcbd5562a0c91a42cc13ccb9ad8374c47133ab6c
ezeyrek55@gmail.com <ezeyrek55@gmail.com>
2022-09-25 10:42:56 +0300

    Autofac desteği eklendi

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,41 @@
+﻿using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<CarManager>().As<ICarService>().SingleInstance();//webApi program.cs de yazılan kodun karşılığı
+            builder.RegisterType<EFCarDal>().As<ICarDal>().SingleInstance();
+
+            builder.RegisterType<ColorManager>().As<IColorService>().SingleInstance();
+            builder.RegisterType<EFColorDal>().As<IColorDal>().SingleInstance();
+
+
+            builder.RegisterType<BrandManager>().As<IBrandService>().SingleInstance();
+            builder.RegisterType<EFBrandDal>().As<IBrandDal>().SingleInstance();
+
+
+            builder.RegisterType<CustomerManager>().As<ICustomersService>().SingleInstance();
+            builder.RegisterType<EFCustomerDal>().As<ICustomerDal>().SingleInstance();
+
+
+            builder.RegisterType<UserManager>().As<IUsersService>().SingleInstance();
+            builder.RegisterType<EFUserDal>().As<IUserDal>().SingleInstance();
+
+
+            builder.RegisterType<RentalManager>().As<IRentalService>().SingleInstance();
+            builder.RegisterType<EFRentalDal>().As<IRentalDal>().SingleInstance();
+        }
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,11 +1,18 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
+using Business.DependencyResolvers.Autofac;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory()); //autofac'i kullan
+
+builder.Host.ConfigureContainer<ContainerBuilder>(builder => builder.RegisterModule( new AutofacBusinessModule()));
 
 builder.Services.AddControllers();
 //eðer data tutmuyorsak Singleton kullanýrýz
@@ -15,18 +22,18 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 //AOP
-builder.Services.AddSingleton<ICarService,CarManager>();// biri constructor da ICarService isterse ona arka planda carManager new e ver
-builder.Services.AddSingleton<ICarDal, EFCarDal>();
-builder.Services.AddSingleton<IBrandService, BrandManager>();
-builder.Services.AddSingleton<IBrandDal, EFBrandDal>();
-builder.Services.AddSingleton<IColorService, ColorManager>();
-builder.Services.AddSingleton<IColorDal, EFColorDal>();
-builder.Services.AddSingleton<ICustomersService,CustomerManager>();
-builder.Services.AddSingleton<ICustomerDal, EFCustomerDal>();
-builder.Services.AddSingleton<IRentalService, RentalManager>();
-builder.Services.AddSingleton<IRentalDal,EFRentalDal>();
-builder.Services.AddSingleton<IUsersService,UserManager>();
-builder.Services.AddSingleton<IUserDal,EFUserDal>();
+//builder.Services.AddSingleton<ICarService,CarManager>();// biri constructor da ICarService isterse ona arka planda carManager new e ver
+//builder.Services.AddSingleton<ICarDal, EFCarDal>();
+//builder.Services.AddSingleton<IBrandService, BrandManager>();
+//builder.Services.AddSingleton<IBrandDal, EFBrandDal>();
+//builder.Services.AddSingleton<IColorService, ColorManager>();
+//builder.Services.AddSingleton<IColorDal, EFColorDal>();
+//builder.Services.AddSingleton<ICustomersService,CustomerManager>();
+//builder.Services.AddSingleton<ICustomerDal, EFCustomerDal>();
+//builder.Services.AddSingleton<IRentalService, RentalManager>();
+//builder.Services.AddSingleton<IRentalDal,EFRentalDal>();
+//builder.Services.AddSingleton<IUsersService,UserManager>();
+//builder.Services.AddSingleton<IUserDal,EFUserDal>();
 
 var app = builder.Build();
 
